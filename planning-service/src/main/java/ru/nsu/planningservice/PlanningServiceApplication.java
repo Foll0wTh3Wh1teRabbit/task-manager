4a6eeb9aabe6a583d3f7769bd8fe8cf86d98d6e8
Nikita Kosarev <51489085+Foll0wTh3Wh1teRabbit@users.noreply.github.com>
2024-12-19 15:39:52 +0700

    Created configuration files for different profiles

--- planning-service/src/main/java/ru/nsu/planningservice/PlanningServiceApplication.java
+++ planning-service/src/main/java/ru/nsu/planningservice/PlanningServiceApplication.java
@@ -2,8 +2,13 @@ package ru.nsu.planningservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class
+    }
+)
 public class PlanningServiceApplication {
 
     public static void main(String[] args) {
